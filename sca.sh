e500bd074616786ed457be9a0f901c8e8472640a
David Eagen <deagen@mdsol.com>
2021-02-04 14:40:34 -0600

    Fix sca.sh to run fossa after install

--- sca.sh
+++ sca.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-curl -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/fossas/fossa-cli/master/install.sh | bash fossa
+curl -H 'Cache-Control: no-cache' https://raw.githubusercontent.com/fossas/fossa-cli/master/install.sh | bash
+fossa
